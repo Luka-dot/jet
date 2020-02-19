2e52cc7d7b609671e175bee0f53c06de959a7f08
Katy Andersen <katherineandersen@sandiego.edu>
2020-02-19 14:33:08 -0500

    engage secondary engines function added

--- rocket.js
+++ rocket.js
@@ -2,5 +2,9 @@ var Rocket = function (color, size) {
     this.color = color;
     this.size = size;
   }
-  
+
   module.exports = Rocket;
+
+  this.engageSecondaryEngines  = function () {
+  console.log('Secondary Engines Engaged!');
+};
